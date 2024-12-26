a398d9e8774be4c9ff0b97d8092cef56c2070675
Cloud-Hosting <arimonox22@gmail.com>
2024-12-26 13:33:57 +0700

    Update

--- v1/server.js
+++ v1/server.js
@@ -1,7 +1,8 @@
 // Menangkap pesan yang dikirim dari main thread
 onmessage = function(event) {
     console.log('Received from main thread:', event.data);
-
+    console.log("disk")
     // Kirim balasan kembali ke main thread
     postMessage('Hello from Worker!');
 };
+
