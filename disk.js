02c10083853a6ce3882b44e5105d5a153d8b7fd0
Cloud-Hosting <arimonox22@gmail.com>
2024-12-27 09:55:18 +0700

    Update

--- disk.js
+++ disk.js
@@ -1,15 +1,17 @@
 const block = {
-    level:function(){
-       const data =  Array(12).fill(
-        Array(31).fill([])
-       )
+    level: function () {
+        const data = Array(12).fill(
+            Array(31).fill([])
+        )
 
-       console.log(data)
+        console.log(data)
     },
-    ad:function(){
-       const th =["time","from","category","subcategory","quantity","price","fee","exprice","note"]
+    tb: function () {
+        const th = ["time", "from", "category", "subcategory", "quantity", "price", "fee", "exprice", "note"]
+        const td = Array(th.length).fill([])
+        console.log(td)
     }
-   
+
 }
 
-block.level()
+block.tb()
